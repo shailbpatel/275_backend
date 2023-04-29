9d237276db2bd84990ef349a0cf849f1ddcd9671
shailbpatel <111547100+shailbpatel@users.noreply.github.com>
2023-04-28 23:57:39 -0700

    Master merge (#1)
    * javaDoc comments added
    
    * registration module API added
    
    * test commit
    
    ---------
    
    Co-authored-by: Prit-Lakhani <priiitttt@gmail.com>

--- src/main/java/sjsu/cmpe275/service/EmployerService.java
+++ src/main/java/sjsu/cmpe275/service/EmployerService.java
@@ -5,14 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
 import sjsu.cmpe275.entity.Address;
 import sjsu.cmpe275.entity.Employee;
 import sjsu.cmpe275.entity.Employer;
 import sjsu.cmpe275.repository.EmployerRepository;
 
 import javax.persistence.EntityManager;
-
-
 import java.util.ArrayList;
 
 @Service
@@ -23,33 +22,90 @@ public class EmployerService {
     @Autowired
     private EntityManager entityManager;
 
+    /**
+     * Creates a new Employer with the specified name, description, and address, and saves it to the database.
+     *
+     * @param name        the name of the Employer to create
+     * @param description a description of the Employer
+     * @param street      the street address of the Employer's location
+     * @param city        the city of the Employer's location
+     * @param state       the state of the Employer's location
+     * @param zip         the zip code of the Employer's location
+     * @param capacity
+     * @param email
+     * @param role
+     * @param
+     * @return the created Employer, or null if an Employer with the same name already exists in the database
+     */
     @Transactional
-    public Employer createEmployer(String name, String description, String street, String city, String state, String zip) {
-        if(employerRepository.findByName(name) != null) {
+    public Employer createEmployer(String name, String description, String street, String city, String zip, String state,   String password,String capacity, String email, String role) {
+        if (employerRepository.findByName(name) != null) {
             return null;
         }
         Employer employer = new Employer();
-        employer.setName(name);
-        employer.setDescription(description);
         Address address = new Address(street, city, state, zip);
         employer.setAddress(address);
+        employer.setName(name);
+        employer.setDescription(description);
+        employer.setCapacity(capacity);
+        employer.setEmail(email);
+        employer.setRole(role);
+        employer.setPassword(password);
+        System.out.println("Email :" + email);
+        System.out.println("Name :" + name);
+        System.out.println("capacity :" + capacity);
+        System.out.println("role :" + role);
+        System.out.println("password :" + password);
+
+
+
+
+
+
+        // Set the Employees of the new Employer to an empty ArrayList
         employer.setEmployees(new ArrayList<Employee>());
         Employer savedEmployer = employerRepository.save(employer);
+        System.out.println(savedEmployer);
         entityManager.flush();
         return savedEmployer;
     }
+
+    /**
+     * Retrieves the Employer with the specified ID from the database.
+     *
+     * @param id the ID of the Employer to retrieve
+     * @return the Employer with the specified ID, or null if the Employer was not found in the database
+     */
     public Employer getEmployer(long id) {
         Employer employer = employerRepository.findById(id);
         return employer;
     }
 
+    /**
+     * Returns an Iterable of all Employers in the database.
+     *
+     * @return an Iterable of all Employers in the database
+     */
     public Iterable<Employer> getAllEmployers() {
-//        return null;
-         return employerRepository.findAll();
+        return employerRepository.findAll();
     }
 
+    /**
+     * Updates the specified Employer in the database with the provided information.
+     *
+     * @param employerId  the ID of the Employer to update
+     * @param name        the new name for the Employer (null to keep the existing name)
+     * @param description the new description for the Employer (null to keep the existing description)
+     * @param street      the new street address for the Employer's location (null to keep the existing address)
+     * @param city        the new city for the Employer's location (null to keep the existing address)
+     * @param state       the new state for the Employer's location (null to keep the existing address)
+     * @param zip         the new zip code for the Employer's location (null to keep the existing address)
+     * @param role
+     * @param password
+     * @return the updated Employer
+     */
     @Transactional
-    public Employer updateEmployer(long employerId, String name, String description, String street, String city, String state, String zip) throws Exception {
+    public Employer updateEmployer(long employerId, String name, String description, String street, String city, String state, String zip, String capacity, String email, String role, String password) throws Exception {
         Employer optionalEmployer = employerRepository.findById(employerId);
         if (optionalEmployer == null) {
             throw new Exception("Employer does not exist!");
@@ -88,11 +144,18 @@ public class EmployerService {
         return savedEmployer;
     }
 
+    /**
+     * Deletes the Employer with the specified ID from the database, if it exists and has no associated Employees.
+     *
+     * @param id the ID of the Employer to delete
+     * @return the deleted Employer, or null if the Employer was not found
+     * @throws ResponseStatusException if the Employer with the specified ID has associated Employees
+     */
     public Employer deleteEmployer(long id) {
         Employer optionalEmployer = employerRepository.findById(id);
         if (optionalEmployer != null) {
             if (optionalEmployer.getEmployees() != null) {
-                if(!optionalEmployer.getEmployees().isEmpty())
+                if (!optionalEmployer.getEmployees().isEmpty())
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employer has employees and cannot be deleted.");
             }
             employerRepository.delete(optionalEmployer);
