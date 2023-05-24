52ecb03075e4f17824ee6266445df9a8d26d1ae4
Shail Patel <shailbrijeshkumar.patel@sjsu.edu>
2023-05-24 05:32:53 +0000

    test

--- src/main/java/sjsu/cmpe275/service/EmailService.java
+++ src/main/java/sjsu/cmpe275/service/EmailService.java
@@ -26,7 +26,7 @@ public class EmailService {
             mimeMessage.setFrom(new InternetAddress("shailp52@gmail.com"));
             mimeMessage.setSubject("WIORS Account Verification");
             mimeMessage.setText("Dear " + user.getName() + ",\n\nClick on this link to verify your account: \n"
-                    + "http://52.23.102.141:8080/verify/" + user.getToken());
+                    + "http://www.woirs-cmpe275.com:8080/verify/" + user.getToken());
         };
         try {
             mailSender.send(messagePreparator);
